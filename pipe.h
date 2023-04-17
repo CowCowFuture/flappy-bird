c5cc377d6d98c4a9947d49b966dbe02422892640
CowCowFuture <82391199+CowCowFuture@users.noreply.github.com>
2023-04-16 19:42:20 -0500

    Modify Handling of Pipes
    Pipes are now a separate type
    Pipe dimensions are now defined in pipe.h

--- pipe.h
+++ pipe.h
@@ -1,22 +1,30 @@
 #include <raylib.h>
 
-struct pipeset {
-  int rectangle_x, offset;
-};
+#define PIPE_WIDTH 100
+#define PIPE_HEIGHT 600
 
-int check_pipe_collisions(Vector2 position1, float radius, struct pipeset pipes)
+typedef struct pipeset {
+  int rectangle_x, offset;
+} pipeset;
+
+int check_pipe_collisions(Vector2 position1, float radius, pipeset pipes)
 {
-  Rectangle top_pipe = {pipes.rectangle_x, pipes.offset, 100, 600};
-  Rectangle bottom_pipe = {pipes.rectangle_x, pipes.offset + 800, 100, 600};
-  if (CheckCollisionCircleRec(position1, radius, top_pipe) || CheckCollisionCircleRec(position1, radius, bottom_pipe))
+  Rectangle top_pipe = {pipes.rectangle_x, pipes.offset,
+			PIPE_WIDTH, PIPE_HEIGHT};
+  Rectangle bottom_pipe = {pipes.rectangle_x, pipes.offset + 800,
+			   PIPE_WIDTH, PIPE_HEIGHT};
+  if (CheckCollisionCircleRec(position1, radius, top_pipe) ||
+      CheckCollisionCircleRec(position1, radius, bottom_pipe))
     {
       return 1;
     }
   return 0;
 }
 
-void draw_pipes(struct pipeset pipes)
+void draw_pipes(pipeset pipes)
 {
-  DrawRectangle(pipes.rectangle_x, pipes.offset, 100, 600, GREEN);
-  DrawRectangle(pipes.rectangle_x, pipes.offset + 800, 100, 600, BLUE);
+  DrawRectangle(pipes.rectangle_x, pipes.offset,
+		PIPE_WIDTH, PIPE_HEIGHT, GREEN);
+  DrawRectangle(pipes.rectangle_x, pipes.offset + 800,
+		PIPE_WIDTH, PIPE_HEIGHT, BLUE);
 }
