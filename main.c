c62f91325efabd8e10ba3d14369acd40ae89ec6a
CowCowFuture <82391199+CowCowFuture@users.noreply.github.com>
2024-01-31 13:51:40 -0600

    Add pause on death

--- main.c
+++ main.c
@@ -35,39 +35,47 @@
 #define PLAYER_RADIUS 25.0
 #define TERMINAL_VELOCITY 25
 
-Vector2 playerPosition = {128, 128};
+Vector2 playerPosition = { 128, 128 };
 float playerVelocity = 0;
 
 
 char string[26] = "Points: ";
 int score = -1; // Below 0 takes you to the main menu.
+int paused = 0; // 0 for =false= 1 for =true=
 
 pipeset pipes[PIPES];
 
 void gameplay()
 {
-  if (IsKeyPressed(KEY_SPACE))
-    playerVelocity = -10.0;
+  // Physics Loop
+  if (!paused) {
+    if (IsKeyPressed(KEY_SPACE))
+      playerVelocity = -10.0;
 
-  // Gravity only used once, I don't need it to be a macro(?)
-  playerVelocity += 0.5;
-  if (playerVelocity > TERMINAL_VELOCITY)
-    playerVelocity = TERMINAL_VELOCITY;
+    playerVelocity += 0.5;
+    if (playerVelocity > TERMINAL_VELOCITY)
+      playerVelocity = TERMINAL_VELOCITY;
 
-  playerPosition.y += playerVelocity;
+    playerPosition.y += playerVelocity;
+
+    for (int i = 0; i < PIPES; i++) {
+      if (pipes[i].rectangle_x > -PIPE_WIDTH) {
+	pipes[i].rectangle_x -= 5;
+      } else {
+	int lastPipe = (i + PIPES - 1) % PIPES;
+	pipes[i].rectangle_x = pipes[lastPipe].rectangle_x + PIPE_WIDTH + PIPE_SPACING;
+	pipes[i].offset = RANDOM_OFFSET;
+	pipes[i].scorable = 1;
+      }
+    }
+  } else {
+    if (IsKeyPressed(KEY_ENTER))
+      score = -1;
+  }
 
   for (int i = 0; i < PIPES; i++) {
-    if (pipes[i].rectangle_x > -PIPE_WIDTH) {
-      pipes[i].rectangle_x -= 5;
-    } else{
-      int lastPipe = (i + PIPES - 1) % PIPES;
-      pipes[i].rectangle_x = pipes[lastPipe].rectangle_x + PIPE_WIDTH + PIPE_SPACING;
-      pipes[i].offset = RANDOM_OFFSET;
-      pipes[i].scorable = 1;
-    }
-
     if (check_pipe_collisions(playerPosition, PLAYER_RADIUS, pipes[i]) || playerPosition.y > WINDOW_HEIGHT) {
-      score = -1;
+      paused = 1;
     } else if (pipes[i].rectangle_x <= playerPosition.x &&
 	       playerPosition.x <= pipes[i].rectangle_x + PIPE_WIDTH &&
 	       pipes[i].scorable == 1) {
@@ -77,17 +85,23 @@ void gameplay()
     sprintf(string, "Score: %i", score);
   }
 
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawCircleV(playerPosition, PLAYER_RADIUS, GRAY);
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+  DrawCircleV(playerPosition, PLAYER_RADIUS, GRAY);
 
-    // Pipes
-    for (int i = 0; i < PIPES; i++)
-      draw_pipes(pipes[i]);
+  // Pipes
+  for (int i = 0; i < PIPES; i++)
+    draw_pipes(pipes[i]);
 
-    // Player
-    DrawText(string, 10, 10, 50, GRAY);
-    EndDrawing();
+  // Player
+  DrawText(string, 10, 10, 50, GRAY);
+  if (paused) { // Currently the only way to pause is to lose
+    DrawText("You've lost!", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 3, 50, GRAY);
+    // Hardcoded number to "center" fix later
+    DrawText("Press RET to return to the main menu.",
+	     WINDOW_WIDTH / 2 - 500, WINDOW_HEIGHT / 3 + 100, 50, GRAY);
+  }
+  EndDrawing();
 }
 
 int button_pressed(int x, int y, int width, int height)
@@ -98,7 +112,8 @@ int button_pressed(int x, int y, int width, int height)
 }
 
 void main_menu() {
-  if (button_pressed((WINDOW_WIDTH - 200)/2, (WINDOW_HEIGHT - 100)/2, 200, 100)) {
+  if (button_pressed((WINDOW_WIDTH - 200)/2, (WINDOW_HEIGHT - 100)/2, 200, 100) ||
+      IsKeyPressed(KEY_SPACE)) {
     // Initalize Player
     playerPosition.y = 128;
     playerVelocity = 0;
@@ -107,7 +122,8 @@ void main_menu() {
     for (int i = 0; i < PIPES; i++)
 	pipes[i] = (pipeset){WINDOW_WIDTH + i * (PIPE_WIDTH + PIPE_SPACING), RANDOM_OFFSET, 1};
 
-    score = 0;
+    paused = 0;
+    score = 0; // Begin Game
   }
 
   BeginDrawing();
